39e21b06532c4370051b10245105b5b627570268
jadde <jazmin.coronel@ismem.edu.pe>
2022-05-11 22:02:49 -0500

    custom fields

--- single.php
+++ single.php
@@ -3,11 +3,15 @@
 
 			   <center>
 			   	
-			   		<?php the_post_thumbnail("large"); ?>
+			   		
 
 				   	<h1><?php the_title(); ?></h1>
+				   	 <h4>Resumen: <?php the_field('resumen'); ?></h4>
+				   	<?php the_post_thumbnail("large"); ?>
 
 				    <?php the_content(); ?>
+				    <h4>Fuente: <?php the_field('fuente'); ?></h4>
+				    
 
 			<ul>
 				<li><a href="https://www.facebook.com/">Facebook</a></li>
